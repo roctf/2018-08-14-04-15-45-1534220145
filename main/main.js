0a88a202a02351fc6f01121d5e235d5cd288bbb0
roctf <roctofly@163.com>
2018-09-24 14:25:17 +0800

    taskone successed

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 // Write your cade below:
 module.exports = function main() {
-  
+	var i, ans, num = [];
+	for (i = 0; i < 2;i++)
+	{
+		num.push(arguments[i]);
+	}
+	if(num[0] > num[1])
+		ans = num[0]%num[1];
+	else
+		ans = num[1]%num[0];
+	return ans;
 };
